906ec16724f236d4349c65670568aa79617abd27
incrediblesadi <info@incrediblesadi.com>
2025-03-16 16:03:47 -0400

    Updated homepage for Akilah.io

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,20 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import React from "react";
 
-const client = generateClient<Schema>();
-
-function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
+const App = () => {
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+    <div style={{ textAlign: "center", padding: "50px", fontFamily: "Arial" }}>
+      <h1>Welcome to Akilah.io</h1>
+      <img src="/logo.png" alt="Akilah Logo" width="200" />
+      <p>Your AWS Custom Dashboard</p>
+
+      <nav>
+        <ul>
+          <li><a href="/chat">Chat Interface</a></li>
+          <li><a href="/aws-resources">AWS System Overview</a></li>
+        </ul>
+      </nav>
+    </div>
   );
-}
+};
 
 export default App;
